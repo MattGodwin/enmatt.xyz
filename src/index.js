f09d81dac2f60fc585a0357b16756af0aee11c3c
Matt <68249412+MattGodwin@users.noreply.github.com>
2021-03-02 18:02:49 +0000

    title and holder stuff

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Title />
+    <Text />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Title() {
+
+    return (
+        <div className='title'>
+            <h1>Enmatt</h1>
+        </div>
+    );
+}
+
+function Text() {
+
+    return(
+        <div className='text'>
+            This is a simple Reactjs website.
+        </div>
+    );
+}
+
+export default Title;
