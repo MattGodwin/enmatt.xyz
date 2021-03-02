ae49cae8232610485f1dbf585c69df3e1a598539
Matt <68249412+MattGodwin@users.noreply.github.com>
2021-03-02 18:53:05 +0000

    done for now

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 import './index.css';
 import ReactDOM from 'react-dom';
 
@@ -6,6 +6,7 @@ ReactDOM.render(
   <React.StrictMode>
     <Title />
     <Text />
+    <SocialMedia />
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -23,8 +24,30 @@ function Text() {
 
     return(
         <div className='text'>
-            This is a simple Reactjs website.
+           <p> 
+               I'm a student who enjoys programming and running various different game servers. My favourite language right now is Python, although I am currently learning JavaScript with React.
+           </p>
+           <p>
+               My social media:
+           </p>
         </div>
+    );
+}
+
+function SocialMedia() {
+
+    return(
+        <ul className="socialmedia">
+            <li className="github">
+                <a href="https://github.com/MattGodwin">github</a>
+            </li>
+            <li className="twitter">
+                <a href="https://twitter.com/ExcitedOcelot">twitter</a>
+            </li>
+            <li className="discord">
+                <a href="https://discord.gg/5H5mKnEqX9">discord</a>
+            </li>
+        </ul>
     );
 }
 
